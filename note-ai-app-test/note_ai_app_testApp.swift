d71277f260d3fe5b3c11fcfb17ab2d4ef8304254
5mars <101134920+5mars@users.noreply.github.com>
2026-03-03 11:35:46 -0500

    Initial Commit

---
+++ note-ai-app-test/note_ai_app_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  note_ai_app_testApp.swift
+//  note-ai-app-test
+//
+//  Created by Jeremy Cinq-Mars on 2026-03-03.
+//
+
+import SwiftUI
+
+@main
+struct note_ai_app_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
